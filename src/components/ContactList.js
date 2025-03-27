c13fcde07df1771dcae9cfbc94e6202bed9746e4
dhanushya-t <dhanushya.t2022cse@sece.ac.in>
2025-03-27 15:58:49 +0530

    update

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,34 +1,20 @@
 import React from "react";
-import ContactCard from "./ContactCard";
-// import { Link } from "react-router-dom";
 
-const ContactList = (props) => {
-    const deleteContactHandler = (id) => {
-        props.getContactId(id);
-    };
-
-    const renderContactList = props.contacts.map((contact) => {
-        return (
-            <ContactCard
-                contact={contact}
-                clickHander={deleteContactHandler}
-                key={contact.id}
-            />
-        );
-    });
-
-    return (
-        <div className="main">
-            <h1>Contact Manager</h1> 
-            <h2>
-                Contact List
-                {/* <Link to="/add">
-                    <button className="ui button blue right">Add Contact</button>
-                </Link> */}
-            </h2>
-            <div className="ui celled list">{renderContactList}</div>
-        </div>
-    );
+const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <div>
+      <h2>Contact List</h2>
+      
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <strong>{contact.name}</strong> - {contact.email} - {contact.phone} 
+            <button onClick={() => onDelete(contact.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ContactList;
