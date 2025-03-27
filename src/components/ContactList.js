8ff4d3061db1ce66566e06345bda0d928928d8a3
dhanushya-t <dhanushya.t2022cse@sece.ac.in>
2025-03-27 10:41:07 +0530

    Added project files

---
+++ src/components/ContactList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+// import { Link } from "react-router-dom";
+
+const ContactList = (props) => {
+    const deleteContactHandler = (id) => {
+        props.getContactId(id);
+    };
+
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <ContactCard
+                contact={contact}
+                clickHander={deleteContactHandler}
+                key={contact.id}
+            />
+        );
+    });
+
+    return (
+        <div className="main">
+            <h1>Contact Manager</h1> 
+            <h2>
+                Contact List
+                {/* <Link to="/add">
+                    <button className="ui button blue right">Add Contact</button>
+                </Link> */}
+            </h2>
+            <div className="ui celled list">{renderContactList}</div>
+        </div>
+    );
+};
+
+export default ContactList;
